1d52c9eaf4cfaf9beef94a6d462a449c6db1253b
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-14 15:01:21 -0600

    added Header and ability to access reduxStore to review.js

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,18 +1,27 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import Header from '../Header/Header';
 
 class Review extends Component {
     render() {
         return (
+            <>
+            <Header />
             <div>
                 <h2>Review Your Feedback</h2>
-                <p>Feelings: </p>
-                <p>Understanding: </p>
-                <p>Support: </p>
-                <p>Comments: </p>
+                <p>Feelings: {this.props.reduxState.feelingReducer.feeling}</p>
+                <p>Understanding: {this.props.reduxState.understandingReducer.understanding}</p>
+                <p>Support: {this.props.reduxState.supportReducer.support}</p>
+                <p>Comments: {this.props.reduxState.commentsReducer.comments}</p>
                 <button>Submit</button>
             </div>
+            </>
         )
     }
 }
 
-export default Review;
+const putReduxStateOnProps = (reduxState) => ({
+    reduxState
+})
+
+export default connect(putReduxStateOnProps)(Review);

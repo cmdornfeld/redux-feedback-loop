b9d3d7b830966c7b025ba88d4f824ee333adf1d8
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-14 14:11:02 -0600

    added handleChange function, and function to dispatch info to reducer and move to next page

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -1,22 +1,38 @@
 import React, {Component} from 'react';
-
+import {connect} from 'react-redux';
 import Header from '../Header/Header';
 
 class Feeling extends Component {
+
+    state = {
+        feeling: ''
+    }
+
+    enterFeelingInfoAndMoveForward = () => {
+        this.props.dispatch({type: 'FEELING', payload: this.state});
+        this.props.history.push('/understanding');
+    }
+
+    handleChange = (event) => {
+        console.log('In handle change with:', event.target.value);
+        this.setState({
+            feeling: event.target.value
+        })
+    }
+
     render() {
         return (
             <>
                 <Header />
                 <div>
-                    <form>
-                        <h3>How are you feeling today?</h3>
-                        <input label="Feeling" type="number" placeholder="Feeling" />
-                        <button>Next</button>
-                    </form>
+                    <h3>How are you feeling today?</h3>
+                    <input onChange={(event) => this.handleChange(event)} label="Feeling" type="number" 
+                        placeholder="Feeling" value={this.state.feeling} />
+                    <button onClick={this.enterFeelingInfoAndMoveForward}>Next</button>
                 </div>
             </>
         )
     }
 }
 
-export default Feeling;
+export default connect()(Feeling);

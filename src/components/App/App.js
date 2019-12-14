3a11fc7138f9f8efb9b95ac66eeb7a9ae3a15f91
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-14 07:25:51 -0600

    added Component folders and files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 
+import Header from '../Header/Header';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
+        <Header />
         <br/>
       </div>
     );

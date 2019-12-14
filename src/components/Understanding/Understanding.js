ca26194af843759494391d40363bc426346b66d0
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-14 14:29:35 -0600

    added functions and state to Understanding component

--- src/components/Understanding/Understanding.js
+++ src/components/Understanding/Understanding.js
@@ -1,22 +1,38 @@
 import React, {Component} from 'react';
-
+import {connect} from 'react-redux';
 import Header from '../Header/Header';
 
 class Understanding extends Component {
+
+    state = {
+        understanding: ''
+    }
+
+    enterUnderstandingInfoAndMoveForward = () => {
+        this.props.dispatch({type: 'UNDERSTANDING', payload: this.state});
+        this.props.history.push('/support');
+    }
+
+    handleChange = (event) => {
+        console.log('In handle change with:', event.target.value);
+        this.setState({
+            understanding: event.target.value
+        })
+    }
+
     render() {
         return (
             <>
                 <Header />
                 <div>
-                    <form>
-                        <h3>How well are you understaning the content?</h3>
-                        <input label="understanding" type="number" placeholder="Understanding" />
-                        <button>Next</button>
-                    </form>
+                    <h3>How well are you understaning the content?</h3>
+                    <input onChange={(event) => this.handleChange(event)} label="understanding" type="number" 
+                    placeholder="Understanding" value={this.state.understanding}  />
+                    <button onClick={this.enterUnderstandingInfoAndMoveForward}>Next</button>
                 </div>
             </>
         )
     }
 }
 
-export default Understanding;
+export default connect()(Understanding);

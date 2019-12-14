cc1465e85def3b15a5995c997631c5ab41c9d17f
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-14 08:55:33 -0600

    imported router and made each component a separate route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {connect} from 'react-redux';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 
-import Header from '../Header/Header';
 import Comments from '../Comments/Comments';
 import Feeling from '../Feeling/Feeling';
 import Supported from '../Supported/Supported';
@@ -14,19 +15,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <br/>
-        <Comments />
-        <br/>
-        <Feeling />
-        <br/>
-        <Supported />
-        <br/>
-        <Understanding />
-        <br/>
-        <Review />
-        <br/>
-        <Admin />
+        <Router>
+          <Route exact path="/" render={(props) => <Feeling {...props} />} />
+          <Route path="/comments" render={(props) => <Comments {...props} />} />
+          <Route path="/support" render={(props) => <Supported {...props} />} />
+          <Route path="/understanding" render={(props) => <Understanding {...props} />} />
+          <Route path="/review" render={(props) => <Review {...props} />} />
+          <Route path="/admin" render={(props) => <Admin {...props} />} />
+        </Router>
       </div>
     );
   }

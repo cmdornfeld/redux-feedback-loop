2d661182332108b02dcca879873e2cf4c518ef6e
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-14 15:52:54 -0600

    added postFeedback post request to review.js

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,8 +1,25 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import {connect} from 'react-redux';
 import Header from '../Header/Header';
 
 class Review extends Component {
+
+    postFeedback = () => {
+        let feedback = {
+            feeling: this.props.reduxState.feelingReducer.feeling,
+            understanding: this.props.reduxState.understandingReducer.understanding,
+            support: this.props.reduxState.supportReducer.support,
+            comments: this.props.reduxState.commentsReducer.comments
+        }
+
+        axios.post('/feedback', feedback).then(response => {
+            this.props.history.push('/success');
+        }).catch(error => {
+            console.log(error);
+            alert('Error adding feedback.  See console for details');
+        })
+    }
     render() {
         return (
             <>
@@ -13,7 +30,7 @@ class Review extends Component {
                 <p>Understanding: {this.props.reduxState.understandingReducer.understanding}</p>
                 <p>Support: {this.props.reduxState.supportReducer.support}</p>
                 <p>Comments: {this.props.reduxState.commentsReducer.comments}</p>
-                <button>Submit</button>
+                <button onClick={this.postFeedback}>Submit</button>
             </div>
             </>
         )

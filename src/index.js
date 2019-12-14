d111e981015efd2bc45a56e808a41f54e9ddcd52
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-14 14:09:00 -0600

    added feelingReducer and setup reduxStore

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const feelingReducer = (state=[], action) => {
+    if (action.type === 'FEELING'){
+        return action.payload
+    }
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        feelingReducer
+    }),
+    applyMiddleware(logger)
+)
+
+ReactDOM.render(<Provider store={storeInstance}> <App /></Provider>, document.getElementById('root'));

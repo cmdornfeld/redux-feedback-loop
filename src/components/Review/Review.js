cb3a887fb945146b4eeaf55db7c964af32ed32c5
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-14 19:50:13 -0600

    added materialUI Card and Button to review.js

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -2,6 +2,22 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import {connect} from 'react-redux';
 import Header from '../Header/Header';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+
+const styles = theme => ({
+    card: {
+      minWidth: 256,
+      fontSize: 28,
+    },
+      textField: {
+        marginLeft: theme.spacing.unit,
+      },
+      button: {
+        margin: theme.spacing.unit,
+      },
+  });
 
 class Review extends Component {
 
@@ -29,15 +45,17 @@ class Review extends Component {
         return (
             <>
             <Header />
-            <div>
+            <Card className={this.props.classes.card}>
                 <h2>Review Your Feedback</h2>
                 <p>Feelings: {this.props.reduxState.feelingReducer.feeling}</p>
                 <p>Understanding: {this.props.reduxState.understandingReducer.understanding}</p>
                 <p>Support: {this.props.reduxState.supportReducer.support}</p>
                 <p>Comments: {this.props.reduxState.commentsReducer.comments}</p>
-                <button onClick={this.returnToPreviousPage}>Back</button>
-                <button onClick={this.postFeedback}>Submit</button>
-            </div>
+                <Button className={this.props.classes.button} onClick={this.returnToPreviousPage}
+                    variant="contained" color="secondary">Back</Button>
+                <Button className={this.props.classes.button} onClick={this.postFeedback}
+                    variant="contained" color="primary">Submit</Button>
+            </Card>
             {JSON.stringify(this.props.reduxState)}
             {/* REMOVE THE ABOVE LINE; FOR TESTING ONLY */}
             </>
@@ -49,4 +67,4 @@ const putReduxStateOnProps = (reduxState) => ({
     reduxState
 })
 
-export default connect(putReduxStateOnProps)(Review);
+export default connect(putReduxStateOnProps)(withStyles(styles)(Review));

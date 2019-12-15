1bae03dcc0b9e00303a69c89bc6cc3c4c326055e
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-15 17:07:58 -0600

    added code comments to feedback.router.js

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+// Receives payload/data from postFeedback function in Review.js
 router.post('/', (req, res) => {
     const newFeedback = req.body;
     const sqlText = `INSERT INTO feedback (feeling, understanding, support, comments) 
         VALUES ($1, $2, $3, $4)`;
+    // Request to send our payload/data (newFeedback/req.body) to our database
+    // the sqlText informs the database that the info will be inserted into the feedback table
+    // and the newFeedback.[property] indicates the value to insert for that property of the table
     pool.query(sqlText, [newFeedback.feeling, newFeedback.understanding, newFeedback.support, newFeedback.comments])
       .then((result) => {
         console.log(`Added feedback to the database`, newFeedback);

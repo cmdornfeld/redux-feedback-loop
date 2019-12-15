eae8cbb6050ae01af7acf2179e86f341fe46e220
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-14 19:39:33 -0600

    added materialUI Card, Textfield and Button to comments.js

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,6 +1,23 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Header from '../Header/Header';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+    card: {
+      minWidth: 256,
+      fontSize: 28,
+    },
+      textField: {
+        marginLeft: theme.spacing.unit,
+      },
+      button: {
+        margin: theme.spacing.unit,
+      },
+  });
 
 class Comments extends Component {
 
@@ -28,13 +45,15 @@ class Comments extends Component {
         return (
             <>
                 <Header />
-                <div>
+                <Card className={this.props.classes.card}>
                     <h3>Any comments you want to leave?</h3>
-                    <input onChange={(event) => this.handleChange(event)} label="comments" type="text" 
-                        placeholder="Enter comments" value={this.state.comments} maxLength="70" />
-                    <button onClick={this.returnToPreviousPage}>Back</button>
-                    <button onClick={this.enterCommentsInfoAndMoveForward}>Next</button>
-                </div>
+                    <TextField required label="Required" className={this.props.classes.textField} onChange={(event) => this.handleChange(event)} 
+                        label="comments" type="text" placeholder="Enter comments" value={this.state.comments} variant="outlined" maxLength="70" />
+                    <Button className={this.props.classes.button} onClick={this.returnToPreviousPage}
+                        variant="contained" color="secondary">Back</Button>
+                    <Button className={this.props.classes.button} onClick={this.enterCommentsInfoAndMoveForward}
+                        variant="contained" color="primary">Next</Button>
+                </Card>
                 {JSON.stringify(this.props.reduxState)}
             </>
         )
@@ -46,4 +65,4 @@ const putReduxStateOnProps = (reduxState) => ({
     reduxState
 })
 
-export default connect(putReduxStateOnProps)(Comments);
+export default connect(putReduxStateOnProps)(withStyles(styles)(Comments));
